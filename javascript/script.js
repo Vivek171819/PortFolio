9b639429358c393933f0793ed094f0b122f6ffd6
Vivek171819 <shewalevivek07@gmail.com>
2024-10-13 23:17:50 +0530

    add Portfolio

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+// document.addEventListener("DOMContentLoaded" ,function(){
+//     window.onscroll=function(){
+//         scrollfunction();
+//     };
+
+//     function scrollfunction(){
+//         if(
+//             document.body.scrollTop > 520 ||
+//             document.documentElement.scrollTop > 520
+//         ){
+//             document.getElementById("nav").style.backgroundColor="#F5F5F5";
+//             document.getElementById("nav").style.transition="0.4s ease-in-out all";
+//         }
+//         else{
+//             document.getElementById("nav").style.backgroundColor="transparent";
+//             document.getElementById("nav").style.transition="0.4s ease-in-out all";
+//         }
+//     }
+//     });
+
+    
+  AOS.init();
+
